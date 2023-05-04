2227c4bdebfe5800f1b17be232f1209440e2aa65
Aqshal Marta Yudha <18523127@students.uii.ac.id>
2023-05-05 00:02:49 +0700

    get id kendaraan

--- app/Http/Controllers/KendaraanController.php
+++ app/Http/Controllers/KendaraanController.php
@@ -36,6 +36,7 @@ class KendaraanController extends Controller
             $motor->mesin_motor = $request->input('mesin_motor');
             $motor->suspensi_motor = $request->input('suspensi_motor');
             $motor->transmisi_motor = $request->input('transmisi_motor');
+            $motor->id_kendaraan = $kendaraan->_id;
             $motor->save();
             $kendaraan->jeniskendaraan()->save($motor);
         }
@@ -45,6 +46,7 @@ class KendaraanController extends Controller
             $mobil->mesin_mobil = $request->input('mesin_mobil');
             $mobil->kapasitas_mobil = $request->input('kapasitas_mobil');
             $mobil->tipe_mobil = $request->input('tipe_mobil');
+            $mobil->id_kendaraan = $kendaraan->_id;
             $mobil->save();
             $kendaraan->jeniskendaraan()->save($mobil);
         }  
@@ -57,20 +59,6 @@ class KendaraanController extends Controller
     
     public function update(Request $request, $id)
     {
-        $motor = new Motor;
-        $motor->mesin_motor = $request->mesin_motor;
-        $motor->suspensi_motor = $request->suspensi_motor;
-        $motor->transmisi_motor = $request->transmisi_motor;
-        $motor->motor_id = $request->input('motor_id');
-        $motor->save();
-        
-        $mobil = new Mobil;
-        $mobil->mesin_mobil = $request->mesin_mobil;
-        $mobil->kapasitas_mobil = $request->kapasitas_mobil;
-        $mobil->tipe_mobil = $request->tipe_mobil;
-        $mobil->mobil_id = $request->input('mobil_id');
-        $mobil->save();
-        
         $kendaraan = new Kendaraan();
         $kendaraan->nama = $request->input('nama');
         $kendaraan->tahun = $request->input('tahun');
@@ -78,6 +66,25 @@ class KendaraanController extends Controller
         $kendaraan->harga = $request->input('harga');
         $kendaraan->stok = $request->input('stok');
         $kendaraan->save();
+        
+        if ($request->filled('mesin_motor') && $request->filled('suspensi_motor') && $request->filled('transmisi_motor')) {
+            $motor = new Motor;
+            $motor->mesin_motor = $request->input('mesin_motor');
+            $motor->suspensi_motor = $request->input('suspensi_motor');
+            $motor->transmisi_motor = $request->input('transmisi_motor');
+            $motor->save();
+            $kendaraan->jeniskendaraan()->save($motor);
+        }
+        
+        if ($request->filled('mesin_mobil') && $request->filled('kapasitas_mobil') && $request->filled('tipe_mobil')) {
+            $mobil = new Mobil;
+            $mobil->mesin_mobil = $request->input('mesin_mobil');
+            $mobil->kapasitas_mobil = $request->input('kapasitas_mobil');
+            $mobil->tipe_mobil = $request->input('tipe_mobil');
+            $mobil->save();
+            $kendaraan->jeniskendaraan()->save($mobil);
+        }
+
         return response()->json($kendaraan);
     }
     
